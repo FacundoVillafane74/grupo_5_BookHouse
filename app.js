fa35152dc67411e974ce2583f6fcee64a99d5322
Facundo Villafañe <facundoagustinvillafane@gmail.com>
2023-07-01 21:00:41 -0300

    creando estructuras de archivos y levantando el servidor con express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const pathPublic = path.join(__dirname, './public');
+app.use(express.static(pathPublic));
+
+app.get('/', (req, res) => {
+    const ruta = path.join(__dirname, './views/index.html');
+    res.sendFile(ruta);
+})
+
+app.listen(3000, () => {
+    console.log('servidor con el puerto 3000 funcionando');
+});
