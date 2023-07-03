36ef56b23a06538435cca6f6d8f75fda2ada2bff
celidonnet <celdnnt@gmail.com>
2023-07-02 23:24:36 -0300

    Armando la estructura del carrito de compras

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     const ruta = path.join(__dirname, './views/index.html');
     res.sendFile(ruta);
 })
+app.get('/cart', (req, res) => {
+    const ruta = path.join(__dirname, './views/productCart.html');
+    res.sendFile(ruta);
+})
 
 app.listen(3000, () => {
     console.log('servidor con el puerto 3000 funcionando');
