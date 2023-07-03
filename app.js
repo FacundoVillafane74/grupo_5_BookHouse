4424dc69787fcbcf0662958875e1b2dc43d87886
NicolásPérez22 <nicolasgedlp22@gmail.com>
2023-07-03 00:07:32 -0300

    modifique el html de productDetail

--- app.js
+++ app.js
@@ -10,15 +10,17 @@ app.get('/', (req, res) => {
     const ruta = path.join(__dirname, './views/index.html');
     res.sendFile(ruta);
 })
-app.get('/cart', (req, res) => {
-    const ruta = path.join(__dirname, './views/productCart.html');
-    res.sendFile(ruta);
-})
 
 app.get('/productDetail', (req, res) => {
     const ruta = path.join(__dirname, './views/productDetail.html');
     res.sendFile(ruta);
 })
+app.get('/cart', (req, res) => {
+    const ruta = path.join(__dirname, './views/productCart.html');
+    res.sendFile(ruta);
+})
+
+
 
 app.listen(3000, () => {
     console.log('servidor con el puerto 3000 funcionando');
