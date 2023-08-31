d771389a152b67e21325a3577145339c8d2bf5de
celidonnet <celdnnt@gmail.com>
2023-08-31 19:46:17 -0300

    uuid

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const uuid = require('uuid');
+const { use } = require('../routes/mainRouter');
+
 const usersPath = path.join(__dirname, '../data/users.json');
 
 let model = {
-
+    
 };
 
 module.exports = model;
